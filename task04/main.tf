2a68a7a36a81a66a4bb11072e6bd002b142558a2
Nurgazy A. Sydykov <62715567+nurgazy-sydykov@users.noreply.github.com>
2026-08-23 21:47:53 +0600

    Update main.tf

--- task04/main.tf
+++ task04/main.tf
@@ -77,7 +77,7 @@ resource "azurerm_network_interface" "nic" {
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "internal"
+    name                          = var.ip_configuration_name
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.public_ip.id
