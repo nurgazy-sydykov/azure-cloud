3f6af17fa2692d7949508df1ebb1b778de0c446a
Nurgazy A. Sydykov <nurgazy1@gmail.com>
2026-08-23 18:27:31 +0600

    updated

--- task04/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vm_public_ip" {
-  description = "Public IP address of the Virtual Machine"
-  value       = azurerm_public_ip.public_ip.ip_address
-}
-
-output "vm_fqdn" {
-  description = "Fully Qualified Domain Name (FQDN) for the VM public IP"
-  value       = azurerm_public_ip.public_ip.fqdn
-}
