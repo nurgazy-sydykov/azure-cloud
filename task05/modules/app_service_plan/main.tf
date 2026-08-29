02685e7aa78376e9d71168b894b07755d46666e1
Nurgazy A. Sydykov <nurgazy1@gmail.com>
2026-08-30 00:02:23 +0600

    worker_count fixed

--- task05/modules/app_service_plan/main.tf
+++ task05/modules/app_service_plan/main.tf
@@ -4,10 +4,10 @@ resource "azurerm_app_service_plan" "this" {
   resource_group_name = var.resource_group_name
 
   sku {
-    tier = "Standard"
-    size = var.sku_name
+    tier     = "Standard"
+    size     = var.sku_name
+    capacity = var.worker_count
   }
 
-  worker_count = var.worker_count
-  tags         = var.tags
+  tags = var.tags
 }
