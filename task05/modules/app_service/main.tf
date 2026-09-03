7e881f9cc56bf605008651e2ec47ba1b730a2f73
Nurgazy A. Sydykov <nurgazy1@gmail.com>
2026-09-04 00:11:11 +0600

    updated

--- task05/modules/app_service/main.tf
+++ task05/modules/app_service/main.tf
@@ -7,14 +7,14 @@ resource "azurerm_windows_web_app" "this" {
   site_config {
     ip_restriction {
       name       = "allow-ip"
-      ip_address = var.allowed_ip
+      ip_address = "18.153.146.156/32" #var.allowed_ip
       action     = "Allow"
       priority   = 100
     }
 
     ip_restriction {
       name        = "allow-tm"
-      service_tag = var.allowed_service_tag
+      service_tag = "AzureTrafficManager" #var.allowed_service_tag
       action      = "Allow"
       priority    = 200
     }
