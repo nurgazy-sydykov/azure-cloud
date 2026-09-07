b38f404d36c918f2497ff25e0f74a32b890a39e7
Nurgazy Sydykov <nurgazy_sydykov@epam.com>
2026-09-07 15:12:15 +0000

    fmt updated

--- task05/modules/app_service_plan/main.tf
+++ task05/modules/app_service_plan/main.tf
@@ -3,8 +3,8 @@ resource "azurerm_service_plan" "this" {
   location            = var.location
   resource_group_name = var.resource_group_name
 
-  os_type   = "Windows"
-  sku_name  = var.sku_name
+  os_type  = "Windows"
+  sku_name = var.sku_name
 
   tags = var.tags
 }
