9d9cf01ca9947670b802fddfbda126f84776f449
Nurgazy A. Sydykov <nurgazy1@gmail.com>
2026-09-07 23:07:36 +0600

    updated asp

--- task05/modules/app_service_plan/main.tf
+++ task05/modules/app_service_plan/main.tf
@@ -2,9 +2,8 @@ resource "azurerm_service_plan" "this" {
   name                = var.name
   location            = var.location
   resource_group_name = var.resource_group_name
-
-  os_type  = "Windows"
-  sku_name = var.sku_name
-
-  tags = var.tags
+  os_type             = "Windows"
+  sku_name            = var.pricing_tier
+  worker_count        = var.worker_count
+  tags                = var.tags
 }
