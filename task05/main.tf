ca7369fe1bd247d751c899454de5ac3c64633a50
Nurgazy A. Sydykov <nurgazy1@gmail.com>
2026-09-09 19:15:12 +0600

    added all

---
+++ task05/main.tf
@@ -0,0 +1,68 @@
+# -------------------------
+# Resource Groups
+# -------------------------
+module "rg" {
+  source   = "./modules/resource_group"
+  for_each = var.resource_groups
+
+  name     = each.value.name
+  location = each.value.location
+  tags     = { (var.tag_key) = var.tag_value }
+}
+
+# -------------------------
+# App Service Plans
+# -------------------------
+module "asp" {
+  source   = "./modules/app_service_plan"
+  for_each = var.app_service_plans
+
+  name                = each.value.name
+  location            = module.rg[each.value.rg_key].location
+  resource_group_name = module.rg[each.value.rg_key].name
+  sku                 = each.value.sku
+  worker_count        = each.value.worker_count
+  tags                = { (var.tag_key) = var.tag_value }
+}
+
+# -------------------------
+# Windows Web Apps
+# -------------------------
+module "app" {
+  source   = "./modules/app_service"
+  for_each = var.app_services
+
+  name                = each.value.name
+  location            = module.rg[each.value.rg_key].location
+  resource_group_name = module.rg[each.value.rg_key].name
+  app_service_plan_id = module.asp[each.value.asp_key].id
+
+  ip_restrictions = var.ip_restrictions
+
+  tags = { (var.tag_key) = var.tag_value }
+}
+
+# -------------------------
+# Traffic Manager Profile
+# -------------------------
+module "traffic_manager" {
+  source = "./modules/traffic_manager"
+
+  name                = var.traffic_manager.name
+  location            = module.rg["rg3"].location
+  resource_group_name = module.rg["rg3"].name
+  routing_method      = var.traffic_manager.routing_method
+
+  endpoints = {
+    app1 = {
+      name   = "endpoint-app1"
+      target = module.app["app1"].id
+    }
+    app2 = {
+      name   = "endpoint-app2"
+      target = module.app["app2"].id
+    }
+  }
+
+  tags = { (var.tag_key) = var.tag_value }
+}
---
+++ task05/modules/app_service/variables.tf
@@ -0,0 +1,35 @@
+variable "name" {
+  type        = string
+  description = "Name of the App Service"
+}
+
+variable "location" {
+  type        = string
+  description = "Location of the App Service"
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Resource group where the App Service will be created"
+}
+
+variable "app_service_plan_id" {
+  type        = string
+  description = "ID of the App Service Plan to host this App Service"
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Tags to apply to the App Service"
+}
+
+variable "ip_restrictions" {
+  type = list(object({
+    name        = string
+    ip_address  = optional(string)
+    service_tag = optional(string)
+    action      = string
+    priority    = number
+  }))
+  description = "List of IP restriction rules"
+}
