d8bf6505151deedb4ad59e3622d9395772203b4d
Nurgazy A. Sydykov <nurgazy1@gmail.com>
2026-09-09 19:16:11 +0600

    cleared

--- task05/modules/app_service_plan/main.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_service_plan" "this" {
-  name                = var.name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  os_type             = "Windows"
-  sku_name            = var.sku
-  worker_count        = var.worker_count
-  tags                = var.tags
-}
--- task05/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "traffic_manager_fqdn" {
-  description = "The FQDN of the Traffic Manager profile"
-  value       = module.traffic_manager.fqdn
-}
