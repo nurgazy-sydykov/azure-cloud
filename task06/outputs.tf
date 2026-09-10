ebc6468aa0a8d708893f4fcae51312d80d6df71e
Nurgazy A. Sydykov <nurgazy1@gmail.com>
2026-09-10 21:11:55 +0600

    cleared

--- task06/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "sql_server_fqdn" {
-  description = "SQL Server fully qualified domain name"
-  value       = module.sql.sql_server_fqdn
-}
-
-output "app_hostname" {
-  description = "Linux Web App hostname"
-  value       = module.webapp.app_hostname
-}
--- task06/terraform.tfvars
+++
@@ -1,7 +0,0 @@
-name_prefix        = "cmaz-3o15j4kj-mod6"
-region             = "Central India"
-allowed_ip_address = "18.153.146.156"
-sql_admin_username = "sqladminuser"
-tags = {
-  Creator = "nurgazy_sydykov@epam.com"
-}
