859375f51c2cb0f0172ba161dabfe5c012c97e6b
Nurgazy Sydykov <nurgazy_sydykov@epam.com>
2026-09-12 11:36:59 +0000

    fmt updated

--- task07/modules/cdn/main.tf
+++ task07/modules/cdn/main.tf
@@ -5,13 +5,13 @@ resource "azurerm_cdn_frontdoor_profile" "fd_profile" {
 }
 
 resource "azurerm_cdn_frontdoor_endpoint" "fd_endpoint" {
-  name                       = var.fd_endpoint_name
-  cdn_frontdoor_profile_id   = azurerm_cdn_frontdoor_profile.fd_profile.id
+  name                     = var.fd_endpoint_name
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.fd_profile.id
 }
 
 resource "azurerm_cdn_frontdoor_origin_group" "fd_origin_group" {
-  name                       = var.fd_origin_group_name
-  cdn_frontdoor_profile_id   = azurerm_cdn_frontdoor_profile.fd_profile.id
+  name                     = var.fd_origin_group_name
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.fd_profile.id
 
   load_balancing {
     sample_size                 = 4
@@ -26,23 +26,23 @@ resource "azurerm_cdn_frontdoor_origin_group" "fd_origin_group" {
 }
 
 resource "azurerm_cdn_frontdoor_origin" "fd_origin" {
-  name                             = var.fd_origin_name
-  cdn_frontdoor_origin_group_id    = azurerm_cdn_frontdoor_origin_group.fd_origin_group.id
+  name                          = var.fd_origin_name
+  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.fd_origin_group.id
 
-  host_name                        = var.origin_hostname
-  http_port                        = 80
-  https_port                       = 443
-  certificate_name_check_enabled   = false
+  host_name                      = var.origin_hostname
+  http_port                      = 80
+  https_port                     = 443
+  certificate_name_check_enabled = false
 }
 
 resource "azurerm_cdn_frontdoor_route" "fd_route" {
-  name                             = var.fd_route_name
-  cdn_frontdoor_endpoint_id        = azurerm_cdn_frontdoor_endpoint.fd_endpoint.id
-  cdn_frontdoor_origin_group_id    = azurerm_cdn_frontdoor_origin_group.fd_origin_group.id
-  cdn_frontdoor_origin_ids         = [azurerm_cdn_frontdoor_origin.fd_origin.id]
+  name                          = var.fd_route_name
+  cdn_frontdoor_endpoint_id     = azurerm_cdn_frontdoor_endpoint.fd_endpoint.id
+  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.fd_origin_group.id
+  cdn_frontdoor_origin_ids      = [azurerm_cdn_frontdoor_origin.fd_origin.id]
 
-  patterns_to_match                = [var.blob_path]
+  patterns_to_match = [var.blob_path]
 
-  supported_protocols              = ["Http", "Https"]
-  forwarding_protocol              = "MatchRequest"
+  supported_protocols = ["Http", "Https"]
+  forwarding_protocol = "MatchRequest"
 }
--- task07/terraform.tfvars
+++ task07/terraform.tfvars
@@ -1,17 +1,17 @@
-location              = "Central India"
+location = "Central India"
 
-resource_group_name   = "cmtr-3o15j4kj-mod7-rg"
-resource_group_id     = "/subscriptions/8da553a7-8f4c-48a2-8701-dafce0b7b79b/resourceGroups/cmtr-3o15j4kj-mod7-rg"
+resource_group_name = "cmtr-3o15j4kj-mod7-rg"
+resource_group_id   = "/subscriptions/8da553a7-8f4c-48a2-8701-dafce0b7b79b/resourceGroups/cmtr-3o15j4kj-mod7-rg"
 
-storage_account_name  = "cmtr3o15j4kjmod7sa"
-storage_account_id    = "/subscriptions/8da553a7-8f4c-48a2-8701-dafce0b7b79b/resourceGroups/cmtr-3o15j4kj-mod7-rg/providers/Microsoft.Storage/storageAccounts/cmtr3o15j4kjmod7sa"
+storage_account_name = "cmtr3o15j4kjmod7sa"
+storage_account_id   = "/subscriptions/8da553a7-8f4c-48a2-8701-dafce0b7b79b/resourceGroups/cmtr-3o15j4kj-mod7-rg/providers/Microsoft.Storage/storageAccounts/cmtr3o15j4kjmod7sa"
 
-blob_filename         = "blob.txt"
+blob_filename = "blob.txt"
 
-fd_profile_name       = "cmtr-3o15j4kj-mod7-fd-profile"
-fd_profile_sku        = "Standard_AzureFrontDoor"
+fd_profile_name = "cmtr-3o15j4kj-mod7-fd-profile"
+fd_profile_sku  = "Standard_AzureFrontDoor"
 
-fd_endpoint_name      = "cmtr-3o15j4kj-mod7-fd-endpoint"
-fd_origin_group_name  = "cmtr-3o15j4kj-mod7-fd-origin-group"
-fd_origin_name        = "cmtr-3o15j4kj-mod7-fd-origin"
-fd_route_name         = "default"
+fd_endpoint_name     = "cmtr-3o15j4kj-mod7-fd-endpoint"
+fd_origin_group_name = "cmtr-3o15j4kj-mod7-fd-origin-group"
+fd_origin_name       = "cmtr-3o15j4kj-mod7-fd-origin"
+fd_route_name        = "default"
