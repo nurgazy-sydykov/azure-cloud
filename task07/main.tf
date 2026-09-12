0dafc039bf6f173426d8ade7679f3cc1d7fe7281
Nurgazy A. Sydykov <nurgazy1@gmail.com>
2026-09-12 19:39:39 +0600

    main.tf updated

--- task07/main.tf
+++ task07/main.tf
@@ -41,7 +41,7 @@ import {
 
 resource "azurerm_storage_blob" "blob" {
   name                   = var.blob_filename
-  storage_account_name   = azurerm_storage_account.sa.name
+  storage_account_name   = "cmtr3o15j4kjmod7sa"
   storage_container_name = azurerm_storage_container.container.name
   type                   = "Block"
 }
