127206a79f18336e6d1fb00c5bd81a7ae4d30372
Nurgazy A. Sydykov <nurgazy1@gmail.com>
2026-09-13 14:32:41 +0600

    modules/cdn/main.tf updated

--- task07/modules/cdn/main.tf
+++ task07/modules/cdn/main.tf
@@ -42,7 +42,7 @@ resource "azurerm_cdn_frontdoor_route" "fd_route" {
   cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.fd_origin_group.id
   cdn_frontdoor_origin_ids      = [azurerm_cdn_frontdoor_origin.fd_origin.id]
 
-  patterns_to_match = [var.blob_path]
+  patterns_to_match = ["/blob.txt"]
 
   supported_protocols = ["Http", "Https"]
   forwarding_protocol = "MatchRequest"
