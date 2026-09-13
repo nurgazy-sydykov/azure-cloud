8e2f6190c9fe33ac25063de0a0004b300ed007bc
Nurgazy A. Sydykov <nurgazy1@gmail.com>
2026-09-13 17:28:03 +0600

    main.tf updated

--- task07/main.tf
+++ task07/main.tf
@@ -42,5 +42,5 @@ module "cdn" {
   fd_route_name        = var.fd_route_name
 
   origin_hostname = azurerm_storage_account.imported_sa.primary_blob_host
-  blob_path       = local.blob_path
+  blob_path       = "/blob.txt"
 }
