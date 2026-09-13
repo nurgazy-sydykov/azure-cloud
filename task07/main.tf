c726a314f234563c8174a0547c844e120aeaafda
Nurgazy A. Sydykov <nurgazy1@gmail.com>
2026-09-13 11:27:04 +0600

    main.tf and variables.tf updated

--- task07/main.tf
+++ task07/main.tf
@@ -41,6 +41,6 @@ module "cdn" {
   fd_origin_name       = var.fd_origin_name
   fd_route_name        = var.fd_route_name
 
-  origin_hostname = data.azurerm_storage_account.sa_data.primary_blob_host
+  origin_hostname = var.origin_hostname
   blob_path       = local.blob_path
 }
